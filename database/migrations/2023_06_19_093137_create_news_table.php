309492648176b6a5ef651d4ee919a6d878fa78b0
starscy <starscy@rambler.ru>
2023-06-20 18:58:58 +0300

    add some migrations

---
+++ database/migrations/2023_06_19_093137_create_news_table.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('author', 100)->default('guest');
+            $table->string('image', 255)->nullable();
+            $table->text('description' )->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+};
